37c408f6703e849b58ebc1f7e06d5fb555ff9636
danghoanganh36 <danghoanganh36@gmail.com>
2023-06-10 14:43:31 +0700

    Anh - Update Upload Avatar Image in Edit Profile Component

--- src/components/profile/edit-profile.js
+++ src/components/profile/edit-profile.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { InputText } from 'primereact/inputtext';
 import { Image } from "primereact/image";
 import { Button } from "primereact/button";
+import { FileUpload } from 'primereact/fileupload';
 
 const EditProfilePage = () => {
     const profile = {
@@ -23,18 +24,18 @@ const EditProfilePage = () => {
           <form className="edit-profile-details">
 
             <span className="input-new-name p-float-label">
-                <InputText className="p-inputtext-lg" placeholder={profile.name} id="new-name"/>
-                <label htmlFor="new-name">New name: </label>
+              <InputText className="p-inputtext-lg" placeholder={profile.name} id="new-name"/>
+              <label htmlFor="new-name">New name: </label>
             </span>
 
             <span className="input-new-email p-float-label">
-                <InputText className="p-inputtext-lg" placeholder={profile.email} id="new-email"/>
-                <label htmlFor="new-email">New email: </label>
+              <InputText className="p-inputtext-lg" placeholder={profile.email} id="new-email"/>
+              <label htmlFor="new-email">New email: </label>
             </span>
-            
-            <span className="input-new-email p-float-label">
-                  <InputText className="p-inputtext-lg" placeholder={profile.email} id="new-email"/>
-                  <label htmlFor="new-email">New email: </label>
+
+            <label htmlFor="new-avatar">New avatar: </label>
+            <span className="input-new-avatar">
+              <FileUpload className="uploadAva" name="demo[]" url={'/api/upload'} multiple accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>} />
             </span>
 
           </form>
