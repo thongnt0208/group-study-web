4d04a7e10374e8268e542e05a4639703391d6ff8
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2023-06-20 01:39:43 +0700

    Hoang - Add Answers and Comments to discussion-detail.js (dont merge yet - this is for testing)

--- src/components/discussion-detail/discussion-detail.js
+++ src/components/discussion-detail/discussion-detail.js
@@ -1,31 +1,146 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'primereact/button';
-import '../../styles/discussion-detail.scss';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Avatar } from 'primereact/avatar';
+import { Badge } from 'primereact/badge';
+import { Card } from 'primereact/card';
 
-const DiscussionDetail = ({ discussion, onGoBack }) => (
-   <div className='p-d-flex p-flex-column '>
-      <div className='p-d-flex p-flex-column p-mt-4'>
-         <div className='p-d-flex p-flex-column'>
-            <h3 className='p-text-bold'>{discussion.discussionTopic}</h3>
-            <h4>{discussion.discussionContent}</h4>
+const DiscussionDetail = ({ discussion, onGoBack }) => {
+   const [answers, setAnswers] = useState([]);
+   const [answer, setAnswer] = useState('');
+   const [comment, setComment] = useState([]);
+
+   const handleAnswerSubmit = () => {
+      const newAnswer = {
+         answer,
+         comments: [],
+      };
+      setAnswers([...answers, newAnswer]);
+      setAnswer('');
+      setComment([...comment, '']);
+   };
+
+   const handleCommentSubmit = (answerIndex, commentValue) => {
+      const updatedAnswers = [...answers];
+      updatedAnswers[answerIndex].comments.push(commentValue);
+      setAnswers(updatedAnswers);
+
+      const updatedComments = [...comment];
+      updatedComments[answerIndex] = '';
+      setComment(updatedComments);
+   };
+
+   const renderAnswers = () => {
+      return answers.map((answer, index) => (
+         <Card key={index} className=''>
+            <div className='card-container'>
+               <Avatar
+                  image='https://picsum.photos/200'
+                  shape='circle'
+                  size='large'
+                  className='p-mr-2 '
+               />
+               <div className='inline-block p-2'>
+                  <h3 className=''>User Name</h3>
+               </div>
+               <h4 className=''>Answer: {answer.answer}</h4>
+            </div>
+            <div className=''>
+               {answer.comments.map((comment, commentIndex) => (
+                  <div key={commentIndex} className='card-container'>
+                     <Avatar
+                        image='https://picsum.photos/200'
+                        shape='circle'
+                        size='medium'
+                        className='p-mr-2'
+                     />
+                     <div>
+                        <p className='inline-block p-2'>{comment}</p>
+                        <Badge value='Like' className='p-mt-2' />
+                     </div>
+                  </div>
+               ))}
+            </div>
+            <div className='p-card-footer'>
+               <div className='p-inputgroup'>
+                  <Avatar
+                     image='https://picsum.photos/200'
+                     shape='circle'
+                     size='medium'
+                     className='p-mr-2'
+                  />
+                  <InputTextarea
+                     onChange={(e) => {
+                        const updatedComments = [...comment];
+                        updatedComments[index] = e.target.value;
+                        setComment(updatedComments);
+                     }}
+                     value={comment[index]}
+                     className='p-inputtext-filled'
+                     placeholder='Write a comment...'
+                     autoResize
+                  />
+                  <Button
+                     label='Comment'
+                     onClick={() => handleCommentSubmit(index, comment[index])}
+                     className='p-button-secondary'
+                  />
+               </div>
+            </div>
+         </Card>
+      ));
+   };
+
+   return (
+      <div className=''>
+         <div className='p-d-flex p-justify-between p-mb-4'>
+            <h3>{discussion.discussionTopic}</h3>
+            <Button
+               label='Go Back'
+               onClick={onGoBack}
+               className='p-button-primary'
+            />
          </div>
+         <div className='p-mb-4'>
+            <p>{discussion.discussionContent}</p>
+         </div>
+         <div className='p-mb-4'>
+            <h4>Answers:</h4>
+            {renderAnswers()}
+         </div>
+         <Card className='p-shadow-2 p-mb-4'>
+            <div className='p-d-flex p-ai-start p-p-3'>
+               <Avatar
+                  image='https://picsum.photos/200'
+                  shape='circle'
+                  size='large'
+                  className='p-mr-2'
+               />
+               <div>
+                  <InputTextarea
+                     onChange={(e) => setAnswer(e.target.value)}
+                     value={answer}
+                     className='p-inputtext-filled p-mb-3'
+                     placeholder='Write your answer...'
+                     rows={4}
+                     autoResize
+                  />
+                  <Button
+                     label='Submit Answer'
+                     onClick={handleAnswerSubmit}
+                     className='p-button-secondary'
+                  />
+               </div>
+            </div>
+         </Card>
       </div>
-      <div className='button-container'>
-         <Button
-            label='Go Back'
-            onClick={onGoBack}
-            className='p-button-secondary'
-         />
-      </div>
-   </div>
-);
+   );
+};
 
 DiscussionDetail.propTypes = {
    discussion: PropTypes.object.isRequired,
    onGoBack: PropTypes.func.isRequired,
 };
-
-DiscussionDetail.defaultProps = {};
 
 export default DiscussionDetail;
--- src/components/group-detail/group-detail.js
+++ src/components/group-detail/group-detail.js
@@ -113,15 +113,15 @@ const GroupDetail = () => {
       const handleDiscussionClick = () => {
          setSelectedDiscussion(discussion);
       };
+
       return (
          <div className='col-12' onClick={handleDiscussionClick}>
-            <div className='flex flex-column xl:flex-row xl:align-items-start p-4 gap-4'>
+            <div className='flex flex-column xl:flex-row xl:align-items-start p-4 gap-4 shadow-5 cursor-pointer transition-delay-200 transition-colors transition-duration-300 hover:surface-300 hover:text-gray-900'>
                <div className='flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4'>
                   <div className='flex flex-column align-items-center sm:align-items-start gap-3'>
                      <div className='text-2xl font-bold text-900'>
                         {discussion.discussionTopic}
                      </div>
-                     {/* <Rating value={product.rating} readOnly cancel={false}></Rating> */}
                      <div className='flex align-items-center gap-3'>
                         <span className='flex align-items-center gap-2'>
                            <i className='pi pi-pencil'></i>
@@ -129,7 +129,6 @@ const GroupDetail = () => {
                               {discussion.discussionContent}
                            </span>
                         </span>
-                        {/* <Tag value={product.inventoryStatus} severity={getSeverity(product)}></Tag> */}
                      </div>
                   </div>
                   <div className='flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2'>
@@ -139,7 +138,6 @@ const GroupDetail = () => {
                            style={{ fontSize: '2.5rem' }}
                         ></i>
                      </span>
-                     {/* <Button icon="pi pi-shopping-cart" className="p-button-rounded" disabled={product.inventoryStatus === 'OUTOFSTOCK'}></Button> */}
                   </div>
                </div>
             </div>
@@ -185,67 +183,3 @@ GroupDetail.propTypes = {};
 GroupDetail.defaultProps = {};
 
 export default GroupDetail;
-
-// import React, { useState, useEffect } from 'react';
-// import { ProductService } from './service/ProductService';
-// import { Button } from 'primereact/button';
-// import { DataView } from 'primereact/dataview';
-// import { Rating } from 'primereact/rating';
-// import { Tag } from 'primereact/tag';
-
-// export default function PaginationDemo() {
-//     const [products, setProducts] = useState([]);
-
-//     useEffect(() => {
-//         ProductService.getProducts().then((data) => setProducts(data));
-//     }, []);
-
-//     const getSeverity = (product) => {
-//         switch (product.inventoryStatus) {
-//             case 'INSTOCK':
-//                 return 'success';
-
-//             case 'LOWSTOCK':
-//                 return 'warning';
-
-//             case 'OUTOFSTOCK':
-//                 return 'danger';
-
-//             default:
-//                 return null;
-//         }
-//     };
-
-//     const itemTemplate = (product) => {
-//         return (
-//             <div className="col-12">
-//                 <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
-//                     <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`} alt={product.name} />
-//                     <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
-//                         <div className="flex flex-column align-items-center sm:align-items-start gap-3">
-//                             <div className="text-2xl font-bold text-900">{product.name}</div>
-//                             <Rating value={product.rating} readOnly cancel={false}></Rating>
-//                             <div className="flex align-items-center gap-3">
-//                                 <span className="flex align-items-center gap-2">
-//                                     <i className="pi pi-tag"></i>
-//                                     <span className="font-semibold">{product.category}</span>
-//                                 </span>
-//                                 <Tag value={product.inventoryStatus} severity={getSeverity(product)}></Tag>
-//                             </div>
-//                         </div>
-//                         <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
-//                             <span className="text-2xl font-semibold">${product.price}</span>
-//                             <Button icon="pi pi-shopping-cart" className="p-button-rounded" disabled={product.inventoryStatus === 'OUTOFSTOCK'}></Button>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         );
-//     };
-
-//     return (
-//         <div className="card">
-//             <DataView value={products} itemTemplate={itemTemplate} paginator rows={5} />
-//         </div>
-//     )
-// }
