a7797441d4b261510a112afb61708ba4e9a6788f
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-06-29 15:04:22 +0700

    Anh - Fetch Create Group API

--- src/components/group-create/AddGroup.js
+++ src/components/group-create/AddGroup.js
@@ -1,92 +1,110 @@
-import React, { useState } from 'react';
-import { Button } from 'primereact/button';
-import { Dialog } from 'primereact/dialog';
-import { InputText } from 'primereact/inputtext';
+import React, { useState } from "react";
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
 
 export default function AddGroup(props) {
-   const [groupTitle, setGroupTitle] = useState('');
-   const [groupSubTitle, setGroupSubTitle] = useState('');
-   const [groupDescription, setGroupDescription] = useState('');
-   const [show, setShow] = useState(false);
+  const [groupTitle, setGroupTitle] = useState("");
+  const [groupSubTitle, setGroupSubTitle] = useState("");
+  const [groupDescription, setGroupDescription] = useState("");
+  const [show, setShow] = useState(false);
 
-   const handleClose = () => setShow(false);
-   const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-   return (
-      <>
-         <Button
-            severity='success'
-            onClick={handleShow}
-            className='p-button-secondary mb-3 md:mb-0'
-         >
-            +Add Group
-         </Button>
+  const formData = new FormData();
+  formData.append("groupTitle", groupTitle);
+  formData.append("groupSubTitle", groupSubTitle);
+  formData.append("groupDescription", groupDescription);
 
-         <Dialog
-            visible={show}
-            onHide={handleClose}
-            header='Add Group'
-            modal
-            className='p-fluid'
-            footer={
-               <>
-                  <Button
-                     label='Close'
-                     className='p-button-text'
-                     onClick={handleClose}
-                  />
-                  <Button
-                     label='Add'
-                     className='p-button-success'
-                     form='editmodal'
-                  />
-               </>
-            }
-         >
-            <form
-               onSubmit={(e) => {
-                  handleClose();
-                  e.preventDefault();
-                  setGroupTitle('');
-                  setGroupSubTitle('');
-                  setGroupDescription('');
-                  props.newGroup(groupTitle, groupTitle, groupDescription);
-               }}
-               id='editmodal'
-               className=''
-            >
-               <div className='p-field'>
-                  <label htmlFor='groupTitle'>Group Title:</label>
-                  <InputText
-                     id='groupTitle'
-                     placeholder='Enter Your Group Title'
-                     value={groupTitle}
-                     onChange={(e) => setGroupTitle(e.target.value)}
-                     required
-                  />
-               </div>
-               <div className='p-field'>
-                  <label htmlFor='groupSubTitle'>Group Sub Title:</label>
-                  <InputText
-                     id='groupSubTitle'
-                     placeholder='Enter Your Group Sub Title'
-                     value={groupSubTitle}
-                     onChange={(e) => setGroupSubTitle(e.target.value)}
-                     required
-                  />
-               </div>
-               <div className='p-field'>
-                  <label htmlFor='groupDescription'>Group Description:</label>
-                  <InputText
-                     id='groupDescription'
-                     placeholder='Enter Your Group Description'
-                     value={groupDescription}
-                     onChange={(e) => setGroupDescription(e.target.value)}
-                     required
-                  />
-               </div>
-            </form>
-         </Dialog>
-      </>
-   );
+  fetch('http://localhost:3000/groups', {
+   method: 'POST',
+   body: formData
+ })
+   .then(response => {
+     if (response.ok) {
+       return response.json();
+     }
+     throw new Error('Failed to post data');
+   })
+   .then(data => {
+     console.log('Data posted successfully:', data);
+   })
+   .catch(error => {
+     console.error('Error:', error.message);
+   });
+
+  return (
+    <>
+      <Button
+        severity="success"
+        onClick={handleShow}
+        className="p-button-secondary mb-3 md:mb-0"
+      >
+        +Add Group
+      </Button>
+
+      <Dialog
+        visible={show}
+        onHide={handleClose}
+        header="Add Group"
+        modal
+        className="p-fluid"
+        footer={
+          <>
+            <Button
+              label="Close"
+              className="p-button-text"
+              onClick={handleClose}
+            />
+            <Button label="Add" className="p-button-success" form="editmodal" />
+          </>
+        }
+      >
+        <form
+          onSubmit={(e) => {
+            handleClose();
+            e.preventDefault();
+            setGroupTitle("");
+            setGroupSubTitle("");
+            setGroupDescription("");
+            props.newGroup(groupTitle, groupTitle, groupDescription);
+          }}
+          id="editmodal"
+          className=""
+        >
+          <div className="p-field">
+            <label htmlFor="groupTitle">Group Title:</label>
+            <InputText
+              id="groupTitle"
+              placeholder="Enter Your Group Title"
+              value={groupTitle}
+              onChange={(e) => setGroupTitle(e.target.value)}
+              required
+            />
+          </div>
+          <div className="p-field">
+            <label htmlFor="groupSubTitle">Group Sub Title:</label>
+            <InputText
+              id="groupSubTitle"
+              placeholder="Enter Your Group Sub Title"
+              value={groupSubTitle}
+              onChange={(e) => setGroupSubTitle(e.target.value)}
+              required
+            />
+          </div>
+          <div className="p-field">
+            <label htmlFor="groupDescription">Group Description:</label>
+            <InputText
+              id="groupDescription"
+              placeholder="Enter Your Group Description"
+              value={groupDescription}
+              onChange={(e) => setGroupDescription(e.target.value)}
+              required
+            />
+          </div>
+        </form>
+      </Dialog>
+    </>
+  );
 }
