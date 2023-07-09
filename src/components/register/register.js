d8f5c5b700fa6987c2682c46986949fce1775892
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-07-09 17:25:18 +0700

    Anh - Fix Register Fetching

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -3,19 +3,19 @@ import "../../styles/register.scss";
 import { Image } from "primereact/image";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
-import { useFormik } from "formik";
 import { Toast } from "primereact/toast";
 import { Link } from "react-router-dom";
 
 const apiUrl = process.env.REACT_APP_API_URL;
-// console.log(apiUrl);
 
 const Register = () => {
   const toast = useRef(null);
   const [errorMessage, setErrorMessage] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordAgain, setPasswordAgain] = useState("");
 
   const show = () => {
-    const { username, password } = formik.values;
     toast.current.show({
       severity: "success",
       summary: "Form Submitted",
@@ -23,23 +23,33 @@ const Register = () => {
     });
   };
 
-  const onSubmit = async (data) => {
-    const formData = new FormData();
-    formData.append("username", data.username);
-    formData.append("password", data.password);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const payload = {
+      username: username,
+      password: password
+    };
+  
+    console.log(payload);
 
     try {
       const response = await fetch(`${apiUrl}/users/register`, {
         method: "POST",
-        body: formData,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
       });
-
+  
       if (response.ok) {
         const result = await response.json();
         // Handle the result here (e.g., show success message)
-        if (data.username && data.password && data.passwordAgain) {
+        if (username && password && passwordAgain) {
           show();
-          formik.resetForm();
+          setUsername("");
+          setPassword("");
+          setPasswordAgain("");
         }
       } else {
         const errorData = await response.json();
@@ -52,60 +62,17 @@ const Register = () => {
     }
   };
 
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      password: "",
-      passwordAgain: "",
-    },
-    validate: (data) => {
-      let errors = {};
+  const isFormFieldInvalid = (value) => {
+    return !value;
+  };
 
-      if (data.username) {
-        errors.username = "Username is required.";
-      }
-      if (data.password) {
-        errors.password = "Password is required.";
-      }
-      if (data.passwordAgain) {
-        errors.passwordAgain = "Confirm password is required.";
-      }
-
-      return errors;
-    },
-    onSubmit: onSubmit,
-    // (data) => {
-    //   data.username && data.password && show();
-    //   console.log("hello");
-    //   formik.resetForm();
-    // }
-  });
-
-  const isFormFieldInvalid = (name) =>
-    !!(formik.touched[name] && formik.errors[name]);
-
-  const getFormErrorMessage = (name) => {
-    return isFormFieldInvalid(name) ? (
-      <small className="p-error">{formik.errors[name]}</small>
+  const getFormErrorMessage = (value) => {
+    return isFormFieldInvalid(value) ? (
+      <small className="p-error">Field is required.</small>
     ) : (
       <small className="p-error">&nbsp;</small>
     );
   };
-
-  // return (
-  //   <div className='registerComponentContainer'>
-  //     <Toast ref={toast} />
-  //     <h2 className="registerComponentTitle">Welcome to HTA Group Study website</h2>
-  //     <Image src="logo_notext.png" alt="Image" width="250" />
-  //     <form onSubmit={formik.handleSubmit} className="registerComponentForm">
-  //       <InputText id="username" type="text" className="p-inputtext-lg" placeholder="Email or Username" required="true" />
-  //       <InputText id="password" type="password" className="p-inputtext-lg" placeholder="Password" required="true" />
-  //       <InputText id="passwordAgain" type="password" className="p-inputtext-lg" placeholder="Confirm Password" required="true" />
-  //       <Button label="Register" type="submit" />
-  //       <Link to={'/'}>Have account? Log in</Link>
-  //     </form>
-  //   </div>
-  // )
 
   return (
     <div className="registerComponentContainer">
@@ -114,31 +81,40 @@ const Register = () => {
         Welcome to HTA Group Study website
       </h2>
       <Image src="logo_notext.png" alt="Image" width="250" />
-      <form onSubmit={formik.handleSubmit} className="registerComponentForm">
+      <form onSubmit={handleSubmit} className="registerComponentForm">
         <InputText
           name="username"
           id="username"
           type="text"
           className="p-inputtext-lg"
           placeholder="Email or Username"
-          required="true"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
         />
+        {getFormErrorMessage(username)}
         <InputText
           name="password"
           id="password"
           type="password"
           className="p-inputtext-lg"
           placeholder="Password"
-          required="true"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
         />
+        {getFormErrorMessage(password)}
         <InputText
           name="passwordAgain"
           id="passwordAgain"
           type="password"
           className="p-inputtext-lg"
           placeholder="Confirm Password"
-          required="true"
+          value={passwordAgain}
+          onChange={(e) => setPasswordAgain(e.target.value)}
+          required
         />
+        {getFormErrorMessage(passwordAgain)}
         <Button label="Register" type="submit" />
         <Link to={"/"}>Have an account? Log in</Link>
         {errorMessage && <p className="error-message">{errorMessage}</p>}
@@ -146,7 +122,5 @@ const Register = () => {
     </div>
   );
 };
-
-Register.defaultProps = {};
 
 export default Register;
