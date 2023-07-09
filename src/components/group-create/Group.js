256d112da63cf585f42355ff1279cc47a8c2b5a2
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-07-09 15:48:09 +0700

    Thong - view a group detail UI

--- src/components/group-create/Group.js
+++ src/components/group-create/Group.js
@@ -29,7 +29,7 @@ export default function Group(props) {
       </div>
    );
 
-   const [admin, setAdmin] = useState(['']);;
+   const [admin, setAdmin] = useState(['']);
 
    const apiUrl = process.env.REACT_APP_API_URL;
 
