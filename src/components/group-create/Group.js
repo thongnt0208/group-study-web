191e248e1ec5fc418f0b26eaaad88b41fa1cd971
trungthongnguyen2002@gmail.com <trungthongnguyen2002@gmail.com>
2023-07-09 11:29:44 +0700

    Thong - update view all groups UI

--- src/components/group-create/Group.js
+++ src/components/group-create/Group.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Group(props) {
    const navigate = useNavigate();
@@ -11,12 +12,14 @@ export default function Group(props) {
    const header = (
       <img
          alt='Card'
-         src='https://primefaces.org/cdn/primereact/images/usercard.png'
+         src={props.coverLink == '' ? 'https://primefaces.org/cdn/primereact/images/usercard.png' : props.coverLink}
+         // src={'https://primefaces.org/cdn/primereact/images/usercard.png'}
+         style={{ width: '100%', height: '220px', 'border-radius': '6px 6px 0 0' }}
       />
    );
    const footer = (
       <div className='flex flex-wrap justify-content-end gap-2'>
-         <Button label='View Detail' id='btnGroupDetail' onClick={navToDetail}/>
+         <Button label='View Detail' id='btnGroupDetail' onClick={navToDetail} />
          <Button
             id='btnGroupDelete'
             label='Delete'
@@ -24,6 +27,42 @@ export default function Group(props) {
          />
       </div>
    );
+
+   const [admin, setAdmin] = useState(['']);;
+
+   const apiUrl = process.env.REACT_APP_API_URL;
+
+   let findAdmin = () => {
+      let adminId = props.adminId;
+      const token = 'Bearer ' + localStorage.getItem('token');
+      axios
+         .get(`${apiUrl}/users`, {
+            headers: {
+               'Authorization': token
+            },
+            params: {
+               'profileId': adminId
+            }
+         })
+         .then((admin) => {
+            setAdmin(admin.data)
+         })
+         .catch((err) => {
+            console.log(err);
+         })
+   }
+
+   useEffect(() => {
+      findAdmin();
+      console.log('this group admin: ', admin);
+   }, []);
+
+   const formattedCreatedAt = new Date(admin.createdAt).toLocaleDateString('en-GB', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+   });
+
 
    return (
       <div className='flex flex-column xl:flex-row xl:align-items-start p-4 gap-4'>
@@ -45,9 +84,14 @@ export default function Group(props) {
                   className='md:w-25rem surface-300'
                >
                   <p className='m-0'>
-                     <h3>Description about this group:</h3>
                      {props.groupDescription}
+                     Created by: {admin.name}
                   </p>
+                  <p className='m-0'>
+                     at {formattedCreatedAt}
+                  </p>
+
+
                </Card>
             </div>
          </div>
