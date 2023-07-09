f519233c322bf5adfdd2db8e60577c6f74c52c9e
danghoanganh36@gmail.com <danghoanganh36@gmail.com>
2023-07-09 16:03:01 +0700

    Anh - Fix View Profile && Edit Profile && Delete Profile Fetching

--- src/components/profile/edit-profile.js
+++ src/components/profile/edit-profile.js
@@ -1,32 +1,42 @@
 import React, { useState, useEffect } from "react";
 import "../../styles/profile.scss";
-import PropTypes from "prop-types";
 
 import { InputText } from "primereact/inputtext";
 import { Image } from "primereact/image";
 import { Button } from "primereact/button";
 import { FileUpload } from "primereact/fileupload";
 import { Link } from "react-router-dom";
+import { decodeToken } from "react-jwt";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const EditProfilePage = () => {
   const [profile, setProfile] = useState({ name: "", email: "" });
-  const profileId = "6496bae0c48fe87ef3bcbc3d";
+  const [currentUser, setCurrentUser] = useState();
 
   useEffect(() => {
-    fetchProfileData();
+    const token = localStorage.getItem("token");
+    const decodedToken = decodeToken(token);
+    setCurrentUser(decodedToken);
   }, []);
 
-  const fetchProfileData = async () => {
+  useEffect(() => {
+    if (currentUser) {
+      fetchProfile();
+    }
+  }, [currentUser]);
+
+  const fetchProfile = async () => {
     try {
-      const response = await fetch(
-        `${apiUrl}/users/view-profile?profileId=${profileId}`
-      ); // Change the API endpoint URL accordingly
+      const response = await fetch(`${apiUrl}/users?profileId=${currentUser._id}`);
       if (response.ok) {
         const data = await response.json();
-        setProfile(data);
-        console.log(data);
+        if (Object.keys(data).length !== 0) {
+          setProfile(data);
+          console.log(data);
+        } else {
+          console.error("Empty profile data");
+        }
       } else {
         console.error("Failed to fetch profile data");
       }
@@ -58,20 +68,12 @@ const EditProfilePage = () => {
     // Perform API request to update the profile using the new data and avatar file
     // Example code: You can modify this based on your backend implementation
     const formData = new FormData();
-    formData.append("profileId", profileId);
-    if (newName) {
-      formData.append("name", newName);
-    } else {
-      formData.append("name", profile.name);
-    }
-    if (newEmail) {
-      formData.append("email", newEmail);
-    } else {
-      formData.append("email", profile.email);
-    }
+    formData.append("profileId", currentUser._id);
+    formData.append("name", newName || profile.name);
+    formData.append("email", newEmail || profile.email);
     formData.append("avatarLink", avatarFile);
 
-    fetch(`${apiUrl}/users/edit-profile?profileId=${profileId}`, {
+    fetch(`${apiUrl}/users/edit-profile?profileId=${currentUser._id}`, {
       method: "PATCH",
       body: formData,
     })
@@ -151,53 +153,4 @@ const EditProfilePage = () => {
   );
 };
 
-EditProfilePage.propTypes = {};
-
-EditProfilePage.defaultProps = {};
-
 export default EditProfilePage;
-
-//DRAFT
-// <div className="edit-profile">
-//   <div className="edit-profile-card">
-//     <Image src="logo192.png" alt="Profile" className="profile-picture" />
-//     <form className="edit-profile-details">
-//       <span className="input-new-name p-float-label">
-//         <InputText
-//           className="p-inputtext-lg"
-//           placeholder={profile.name}
-//           id="new-name"
-//         />
-//         <label htmlFor="new-name">New name: </label>
-//       </span>
-
-//       <span className="input-new-email p-float-label">
-//         <InputText
-//           className="p-inputtext-lg"
-//           placeholder={profile.email}
-//           id="new-email"
-//         />
-//         <label htmlFor="new-email">New email: </label>
-//       </span>
-
-//       <span className="input-new-avatar">
-//         <FileUpload
-//           className="uploadAva"
-//           name="demo[]"
-//           url={"/api/upload"}
-//           multiple
-//           accept="image/*"
-//           maxFileSize={1000000}
-//           emptyTemplate={
-//             <p className="m-0">
-//               Drag and drop files to here to upload new avatar file.
-//             </p>
-//           }
-//         />
-//       </span>
-//     </form>
-//     <div className="btn-save">
-//       <Button className="btnSave" label="Save" icon="pi pi-spin pi-sync" />
-//     </div>
-//   </div>
-// </div>
